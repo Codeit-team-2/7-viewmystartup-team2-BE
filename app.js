cccf388d630afd0b8976c041b92913267d6d607e
Woojin <jwj3171@gmail.com>
2025-06-23 16:49:48 +0900

    feat(invesment): Implement POST Investment 성공 #19

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use("/users", userRoutes);
 app.use("/companies", companyRoutes);
 
 // investment 라우터
+//태홍님께 맞출 에정 - 우진
 app.use("/investments", investmentRoutes); // 처음부터 오타낸건지 다른분이 바꾼건지 몰라서 일단주석처리
 
 // myCompanySelection 라우터
--- services/investment/investment.service.js
+++ services/investment/investment.service.js
@@ -3,10 +3,15 @@ import {
   deleteInvestmentRepo,
   getAllInvestmentsFromDB,
   updateInvestmentRepo,
-  postInvestmentsFromDB,
+  findUserById,
+  decrementUserBalance,
+  createInvestment,
+  // postInvestmentsFromDB,
 } from "../../repositories/investment/investment.repository.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export const fetchInvestmentsByCompanyId = async companyId => {
+export const fetchInvestmentsByCompanyId = async (companyId) => {
   const investments = await getAllInvestmentsFromDB(companyId);
   // 순위 계산 로직
   return investments.map((inv, idx) => ({
@@ -26,10 +31,28 @@ export const updateInvestmentService = async (
   });
 };
 
-export const deleteInvestmentService = async investmentId => {
+export const deleteInvestmentService = async (investmentId) => {
   return await deleteInvestmentRepo(investmentId);
 };
 
-export const postInvestments = async (form) => {
-  return await postInvestmentsFromDB(form);
+export const postInvestments = async ({
+  userId,
+  companyId,
+  howMuch,
+  comment,
+  password,
+}) => {
+  const user = await findUserById(userId);
+  if (!user) throw new Error("존재하지 않는 유저입니다.");
+  if (user.password !== password)
+    throw new Error("비밀번호가 일치하지 않습니다.");
+  if (user.balance < howMuch) throw new Error("잔액이 부족합니다.");
+
+  // 트랜잭션으로 처리
+  const [_, investment] = await prisma.$transaction([
+    decrementUserBalance(prisma, userId, howMuch),
+    createInvestment(prisma, { userId, companyId, howMuch, comment }),
+  ]);
+
+  return investment;
 };
