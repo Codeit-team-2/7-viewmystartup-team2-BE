a2f172c8247d81245a4bc5e79c258830be5b8930
Woojin <jwj3171@gmail.com>
2025-06-26 14:40:15 +0900

    feat(auth/refresh): 닉네임으로 user정보최신화 api작성#19

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -1,5 +1,8 @@
 // controllers/auth/auth.controller.js
-import { loginUser } from "../../services/auth/auth.service.js";
+import {
+  loginUser,
+  refreshUserInfo,
+} from "../../services/auth/auth.service.js";
 
 export const handleLogin = async (req, res) => {
   const { nickname, password } = req.body;
@@ -10,5 +13,17 @@ export const handleLogin = async (req, res) => {
   } catch (error) {
     console.error("❌ [handleLogin] error:", error.message);
     res.status(401).json({ error: error.message });
+  }
+};
+
+export const handleRefresh = async (req, res) => {
+  const { nickname } = req.params;
+
+  try {
+    const user = await refreshUserInfo(nickname);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("❌ [handleRefresh] error:", error.message);
+    res.status(404).json({ error: error.message });
   }
 };
--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,9 +1,10 @@
 // routes/auth/auth.routes.js
 import express from "express";
-import { handleLogin } from "../../controllers/auth/auth.controller.js";
+import { handleLogin, handleRefresh} from "../../controllers/auth/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", handleLogin);
+router.get("/refresh/:nickname", handleRefresh);
 
 export default router;
